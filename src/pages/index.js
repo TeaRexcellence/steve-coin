2b4d5aa02f6c65f511fc8ad038ecc32868f31bdf
tearexcellence <tearexcellence@protonmail.com>
2023-05-07 19:07:10 -0700

    Remove ts-check

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import { PrimaryContainer } from '../Templates/Primary-Container'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import { PrimaryContainer } from '../Templates/Primary-Container';
 import { Meta } from '../modules/head/Meta';
 import { GetStaticProps } from 'next';
 import { About } from '../modules/Sections/About';
@@ -10,22 +10,26 @@ import { Roadmap } from '../modules/Sections/Roadmap';
 import { HowToBuy } from '../modules/Sections/HowToBuy';
 import { Hero } from '../modules/Sections/Hero';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-
-      <PrimaryContainer meta={<Meta metaTitle="The bird is the word" metaImg="/favicon.jpg" description="Steve is a community owned meme coin ready to take over the world!" />}>
-      
-      <Hero/>
-      <About/>
-      <Tokenomics/>
-      <Roadmap/>
-      <HowToBuy/>
-
+      <PrimaryContainer
+        meta={
+          <Meta
+            metaTitle="The bird is the word"
+            metaImg="/favicon.jpg"
+            description="Steve is a community owned meme coin ready to take over the world!"
+          />
+        }
+      >
+        <Hero />
+        <About />
+        <Tokenomics />
+        <Roadmap />
+        <HowToBuy />
       </PrimaryContainer>
-
-  </>
-  )
+    </>
+  );
 }
