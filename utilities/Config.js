48f17d3335d1171dfeeb7956347e80dbb78fbb34
tearexcellence <tearexcellence@protonmail.com>
2023-05-07 19:43:28 -0700

    Remove unused env var

--- utilities/Config.js
+++ utilities/Config.js
@@ -9,11 +9,10 @@ function stripUrl(url) {
 }
 
 export const Config = {
-  url: process.env.SITE_URL,
-  stripped_url: process.env.SITE_URL ? stripUrl(process.env.SITE_URL) : '',
   site_name: '$STEVE',
   title: '$STEVE',
-  description: 'Steve is a community owned meme coin ready to take over the world!',
+  description:
+    'Steve is a community owned meme coin ready to take over the world!',
   locale: 'en',
   author: 'Steve community leadership',
   author_image: '/favicon.jpg',
